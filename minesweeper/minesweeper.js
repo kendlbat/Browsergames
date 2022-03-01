e391be9d57aac369cdb86bc8bf08fdb4b4a30716
Tobias Kendlbacher <kendlbat@edu.htl-villach.at>
2022-03-01 16:25:02 +0100

    Fix accidental loss

--- minesweeper/minesweeper.js
+++ minesweeper/minesweeper.js
@@ -88,7 +88,7 @@ class Cell {
             recursive = false;
         }
         
-        if (this.revealed && !recursive) {
+        if (this.revealed && !recursive && this.adjacentMines() == this.adjacentFlagged()) {
             for (let i = -1; i <= 1; i++) {
                 for (let j = -1; j <= 1; j++) {
                     if (i == 0 && j == 0) {
